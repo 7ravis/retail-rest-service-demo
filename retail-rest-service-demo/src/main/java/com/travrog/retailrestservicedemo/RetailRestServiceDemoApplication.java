d89eb8a3fe22b3b20b298b01004bc0f145fb036b
Travis Rogers <7ravis@gmail.com>
2018-06-21 15:33:42 -0500

    add base maven project

---
+++ retail-rest-service-demo/src/main/java/com/travrog/retailrestservicedemo/RetailRestServiceDemoApplication.java
@@ -0,0 +1,12 @@
+package com.travrog.retailrestservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailRestServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailRestServiceDemoApplication.class, args);
+	}
+}
