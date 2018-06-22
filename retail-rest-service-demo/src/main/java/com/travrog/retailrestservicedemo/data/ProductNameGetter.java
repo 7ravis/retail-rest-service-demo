be36404167759cd99b1f68407ee2dd1ddf3ead13
Travis Rogers <7ravis@gmail.com>
2018-06-21 20:05:24 -0500

    add javadoc for ProductNameGetter #getNameByProductId

--- retail-rest-service-demo/src/main/java/com/travrog/retailrestservicedemo/data/ProductNameGetter.java
+++ retail-rest-service-demo/src/main/java/com/travrog/retailrestservicedemo/data/ProductNameGetter.java
@@ -2,6 +2,10 @@ package com.travrog.retailrestservicedemo.data;
 
 public interface ProductNameGetter {
 
+	/**
+	 * @param productId
+	 * @return product name, or null if unable to obtain
+	 */
 	String getNameByProductId(long productId);
 	
 }
