31935d4e4c27bf5d7e65af4f7944d8523c3f8c9b
Travis Rogers <7ravis@gmail.com>
2018-06-24 23:01:03 -0500

    delete classes not part of final design

--- retail-rest-service-demo/src/main/java/com/travrog/retailrestservicedemo/repository/ValidProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.travrog.retailrestservicedemo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.travrog.retailrestservicedemo.domain.product.ClientFacingProduct;
-
-public interface ValidProductRepository extends MongoRepository<ClientFacingProduct, Long> {
-
-}
